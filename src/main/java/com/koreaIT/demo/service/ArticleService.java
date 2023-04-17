1e28a9799c45d840ff5558cb6da3bd2a83783066
KDG <airedale63@naver.com>
2023-04-17 18:10:43 +0900

    mybatis, mysql-connector, repository mapper

--- src/main/java/com/koreaIT/demo/service/ArticleService.java
+++ src/main/java/com/koreaIT/demo/service/ArticleService.java
@@ -17,8 +17,6 @@ public class ArticleService {
 	@Autowired
 	public ArticleService(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
-
-		articleRepository.makeTestData();
 	}
 
 	public Article writeArticle(String title, String body) {
