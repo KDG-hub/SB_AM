69fe8e7728f13c80a7ed7393742ce540a5bb6e38
KDG <airedale63@naver.com>
2023-04-19 01:48:50 +0900

    Utility.empty

--- src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.koreaIT.demo.service.MemberService;
+import com.koreaIT.demo.util.Utility;
 import com.koreaIT.demo.vo.Member;
 
 @Controller
@@ -21,31 +22,33 @@ public class UsrMemberController {
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public String doJoin(String loginId, String loginPw, String loginPwChk , String name, String nickName, String cellphoneNum, String email) {
-		if(loginId == null || loginId.trim().length() == 0) {
+		if(Utility.empty(loginId)) {
 			return "아이디를 입력해주세요";
 		}
-		if(loginPw == null || loginPw.trim().length() == 0) {
+		if(Utility.empty(loginPw)) {
 			return "비밀번호를 입력해주세요";
 		}
-		if(name == null || name.trim().length() == 0) {
+		if(Utility.empty(name)) {
 			return "이름을 입력해주세요";
 		}
-		if(cellphoneNum == null || cellphoneNum.trim().length() == 0) {
+		if(Utility.empty(cellphoneNum)) {
 			return "전화번호를 입력해주세요";
 		}
-		if(nickName == null || nickName.trim().length() == 0) {
+		if(Utility.empty(nickName)) {
 			return "닉네임을 입력해주세요";
 		}
-		if(email == null || email.trim().length() == 0) {
+		if(Utility.empty(email)) {
 			return "이메일을 입력해주세요";
 		}
 		if(loginPw == loginPwChk) {
 			return "비밀번호를 확인해주세요";
 		}
 		
-		memberService.doJoin(loginId, loginPw, name, nickName, cellphoneNum, email);
-			
-		int id = memberService.getLastInsertId();
+		int id = memberService.doJoin(loginId, loginPw, name, nickName, cellphoneNum, email);
+		
+		if(id == -1) {
+			return "중복된 아이디입니다.";
+		}
 		
 		Member member = memberService.getMemberById(id);
 			
