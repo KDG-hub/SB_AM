cce0c6a81b18ff499316ca419ed3b0105d0007fb
KDG <airedale63@naver.com>
2023-04-26 18:18:52 +0900

    interceptor 구현

--- src/main/java/com/koreaIT/demo/vo/Rq.java
+++ src/main/java/com/koreaIT/demo/vo/Rq.java
@@ -1,7 +1,12 @@
 package com.koreaIT.demo.vo;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.koreaIT.demo.util.Utility;
 
 import lombok.Getter;
 
@@ -9,19 +14,37 @@ public class Rq {
 
 	@Getter
 	private int loginedMemberId;
+	private HttpServletRequest req;
+	private HttpServletResponse resp;
 
-	public Rq(HttpServletRequest req) {
-
-		HttpSession httpSession = req.getSession();
-
+	public Rq(HttpServletRequest req, HttpServletResponse resp) {
+		
+		this.req =req;
+		this.resp =resp;
+		
+	HttpSession httpSession = req.getSession();
+		
 		int loginedMemberId = 0;
-
+		
 		if (httpSession.getAttribute("loginedMemberId") != null) {
 			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
 		}
-
+		
 		this.loginedMemberId = loginedMemberId;
 	}
 
+	public void jsPrintHistoryBack(String msg) {
+		resp.setContentType("text/html; charset=UTF-8;");
+		
+		print(Utility.jsHistoryBack(msg));
+	}
+
+	private void print(String str) {
+		try {
+			resp.getWriter().append(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
